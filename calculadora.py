8e8e763006eb2a429efb182e2dec4166600321f3
Miguel Camargo <omiguelcamargo@gmail.com>
2026-02-02 09:58:36 -0300

    Condicionais adicionadas

--- calculadora.py
+++ calculadora.py
@@ -2,3 +2,12 @@ numero1 = input('Digite um número: ')
 numero2 = input('Digite outro número: ')
 
 operador = input('Digite um operador: ')
+
+if operador == '+' or 'somar':
+    print(numero1 + numero2)
+elif operador == '-' or 'subtrair':
+    print(numero1 - numero2)
+elif operador == '*' or 'multiplicar':
+    print(numero1 * numero2)
+elif operador == '/' or 'dividir':
+    print(numero1 / numero2)
