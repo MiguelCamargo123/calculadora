16ecd9a69373f218b1307bb8df554013b6783391
Miguel Camargo <omiguelcamargo@gmail.com>
2026-02-02 10:07:49 -0300

    Loop adicionado e tratamento nos números

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,19 @@
-numero1 = input('Digite um número: ')
-numero2 = input('Digite outro número: ')
+entrar_ou_sair = input('Deseja entrar ou sair? (S/N) ')
 
-operador = input('Digite um operador: ')
+while entrar_ou_sair == 'S':
+    numero1 = int(input('Digite um número: '))
+    numero2 = int(input('Digite outro número: '))
 
-if operador == '+' or 'somar':
-    print(numero1 + numero2)
-elif operador == '-' or 'subtrair':
-    print(numero1 - numero2)
-elif operador == '*' or 'multiplicar':
-    print(numero1 * numero2)
-elif operador == '/' or 'dividir':
-    print(numero1 / numero2)
+    operador = input('Digite um operador: ')
+
+    if operador == '+' or 'somar':
+        print(numero1 + numero2)
+    elif operador == '-' or 'subtrair':
+        print(numero1 - numero2)
+    elif operador == '*' or 'multiplicar':
+        print(numero1 * numero2)
+    elif operador == '/' or 'dividir':
+        print(numero1 / numero2)
+    if operador == 'S':
+        print('Você saiu do programa')
+        break
